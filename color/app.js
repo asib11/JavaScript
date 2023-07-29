e46226afa90313a303ffdbd1de562f292dacb885
asib <ahmedasib1@gmail.com>
2023-07-30 00:12:25 +0600

    color project

--- color/app.js
+++ color/app.js
@@ -1,4 +1,12 @@
 const btn = document.querySelector("button");
+const h1 = document.querySelector("h1")
+
+
 btn.addEventListener("click",()=>{
-    document.body.style.backgroundColor = "green";
+    const r = Math.floor(Math.random() *255);
+    const g = Math.floor(Math.random() *255);
+    const b = Math.floor(Math.random() *255);
+    const newcolor = `rgb( ${r},${g}, ${b})`;
+    document.body.style.backgroundColor = newcolor;
+    h1.innerText = newcolor;
 })
