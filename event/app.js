22ec6ce07175c36b333887e396d2ccf455cac63d
asib <ahmedasib1@gmail.com>
2023-08-11 19:41:48 +0600

    2 type event added

---
+++ event/app.js
@@ -0,0 +1,7 @@
+//type 1 direct
+//<button onclick="document.body.style.backgroundColor = 'red'">red</button>
+
+//type 2
+function makeColorBlue(){
+    document.body.style.backgroundColor = 'blue';
+}
