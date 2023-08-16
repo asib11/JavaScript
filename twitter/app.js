9d5cc47b97dc8b7df2b4df16ba557aa9bd60a766
asib <ahmedasib1@gmail.com>
2023-08-16 11:30:06 +0600

    delete btn work

--- twitter/app.js
+++ twitter/app.js
@@ -8,3 +8,18 @@ btn.addEventListener('click',()=> {
     postList.append(p);
     input.value = '';
 });
+
+document.getElementById("delete").addEventListener("keyup", (event)=>{
+    const text = event.target.value;
+    const deleteBtn = document.getElementById("delete-btn");
+    if (text === 'delete'){
+        deleteBtn.removeAttribute('disabled');
+    } else{
+        deleteBtn.setAttribute('disabled', true);
+    }
+});
+
+document.getElementById('delete-btn').addEventListener('click', ()=>{
+    document.getElementById('h1').style.display = 'none'
+})
+
