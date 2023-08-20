745d5ec964aaafa0410c18c62a8957ea8d8629fb
asib <ahmedasib1@gmail.com>
2023-08-20 23:56:20 +0600

    dom learning

---
+++ dom/selector.js
@@ -0,0 +1,24 @@
+/*
+1.how to use js with html and css
+
+2.get element or elements from dom
+    2-1: getElementById (use many times)
+    2-2: getElementSByClassName
+    2-3: querySelector
+    2-4: querySelectorAll
+
+3. Dynamically add element to the dom: document.createElement
+    3-1: set value to the innnerHTML
+
+4. get and set value from/to a dom element
+    4-1: use innerText (h1, p, h5, div, span, strong)
+    4-2: use value( input, textarea)
+
+5. add event listener (event handler)
+    5-1: click, focus, blur, onchange, keyup
+    5-2: addEventListener, onClick =''
+
+6. use function to reduce dublicate code
+
+7. event bubble and event delegate
+*/
