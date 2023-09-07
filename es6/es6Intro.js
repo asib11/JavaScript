aa3d08dbc4e81fcfcf4a23c3b830d9d11e2f71c0
asib <ahmedasib1@gmail.com>
2023-09-08 00:16:29 +0600

    es6

---
+++ es6/es6Intro.js
@@ -0,0 +1,16 @@
+// 1. var let const
+//break up with var
+const number = [2,3,4];
+let salary = 450; //let changeable
+salary = 455 ;
+
+//2. default parameter
+//3. templet string
+const serial = `
+ asib
+ ahmed
+`;
+//4. arrow function
+const a= b=> b+b ;
+//5. spread
+//6. destructering
