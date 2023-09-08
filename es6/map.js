b9d62bc769c1d1d2a9f80360ea45a54735e45386
asib <ahmedasib1@gmail.com>
2023-09-08 23:55:34 +0600

    chaining

---
+++ es6/map.js
@@ -0,0 +1,9 @@
+const numbers = [1,2,3,4,5];
+const result = numbers.map(x => x**2);
+console.log(result);
+
+//string
+
+const text = ['asib', 'ahmed', 'hamza'];
+const upperText = text.map(x => x[0].toUpperCase()+x.slice(1)); //slice work
+console.log(upperText);
