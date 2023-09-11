95a73f20393aaa3ec1ee450798d3253deda017b4
asib <ahmedasib1@gmail.com>
2023-09-11 16:24:32 +0600

    encapsulations

--- es6/encapsulation.js
+++ es6/encapsulation.js
@@ -2,16 +2,16 @@ class Players{
     #salary // Private field '#salary' must be declared in an enclosing class.
 
     constructor(name, age , salary){
-        this.name = name;
-        this._age = age;
-        this.#salary = salary;
+        this.name = name; //public
+        this._age = age; // protected
+        this.#salary = salary; // private
     }
 
     calculateSalary(){
-        return this.#salary*12
+        return `anual salary ${this.#salary*12}`
     }
 }
 
 const sakib = new Players('sakib', 35, 120000);
 console.log(sakib.calculateSalary());
-console.log(sakib.age);
+console.log(sakib._age);
