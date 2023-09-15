7cdab5b280cc1ad69cecd0560e5ab5cadae20423
asib <ahmedasib1@gmail.com>
2023-09-15 22:59:44 +0600

    country name add

--- api/country/app.js
+++ api/country/app.js
@@ -13,10 +13,24 @@ const displayData = data =>{
         countriesDetails.innerHTML = `
         <h3> name: ${country.name.common} </h3>
         <h2>capital: ${country.capital? country.capital[0]: 'no capital'}</h2>
-        <button class ="btn-name"> details </button>
+        <button onClick = "loadCountriesDetails('${country.cca2}')"class ="btn-name"> details </button>
         `
         countriesName.appendChild(countriesDetails);
     });
+};
+
+const loadCountriesDetails = code =>{
+    const url = `https://restcountries.com/v3.1/alpha/${code}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data =>showFlag(data[0]))
 }
 
+const showFlag = flag =>{
+    const detailsContainer = document.getElementById('country-details');
+    detailsContainer.innerHTML =`
+    <h2>name: ${flag.name.common}</h2>
+    <img src="${flag.flags.png}">
+    `
+}
 countries();
