12727d310dd0defa5f88744fe793f779b22507cb
asib <ahmedasib1@gmail.com>
2023-09-23 23:38:18 +0600

    primitive type and non primitive type define

---
+++ common/primitive.js
@@ -0,0 +1,24 @@
+// static or strongly language
+// int a, string name, arry[] etc define datatype speificaly
+
+//javaScript is a dynamic typed language
+// var let const. no need to define specific datatype
+
+//primitive types
+
+const a = 5;
+const b = 'asib ahmed';
+const c = true;
+let x = 5;
+let y = 5;
+console.log(x,y); //ans 5,5
+// primitive types can reassign data
+y = 8;
+console.log(x,y); //ans 5,8
+
+//non primitive type
+//object, array
+// can not reassign data because they refer same memory
+
+const p = [1,2,3]
+const q ={a:1,b:2}
